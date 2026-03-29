9874169d4bf7dec9bd730c4924d764cacf8c7d77
David Brown <davidb@davidb.org>
2026-03-29 14:54:04 -0600

    Initial Commit

---
+++ Anihortes/AnihortesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnihortesApp.swift
+//  Anihortes
+//
+//  Created by David Jr on 2026-03-29.
+//
+
+import SwiftUI
+
+@main
+struct AnihortesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anihortes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anihortes
+//
+//  Created by David Jr on 2026-03-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
